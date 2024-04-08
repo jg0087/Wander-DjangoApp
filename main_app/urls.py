1d08801e3b5e1fb9ec627cca7b4e593a38ee4cea
Isaiahmill14 <isaiahxmiller@gmail.com>
2024-04-08 19:52:44 -0400

    Added Create, Update, and Delete views and url paths. Created superuser and added model to admin

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('itineraries/', views.itineraries_index, name='index'),
+    path('itineraries/create/', views.ItineraryCreate.as_view(), name='itineraries_create'),
+    path('itineraries/<int:pk>/update/', views.ItineraryUpdate.as_view(), name='itineraries_update'),
+    path('itineraries/<int:pk>/delete/', views.ItineraryDelete.as_view(), name='itineraries_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Itinerary
 
 # Create your views here.
@@ -13,3 +15,21 @@ def itineraries_index(request):
   return render(request, 'itineraries/index.html', {
     'itineraries': itineraries
   })
+
+def itineraries_detail(request, itinerary_id):
+   itinerary = Itinerary.objects.get(id=itinerary_id)
+   return render(request, 'itinerary/detail.html', {
+      'itinerary': itinerary
+   })
+
+class ItineraryCreate(CreateView):
+   model = Itinerary
+   fields = ['name', 'date_from', 'date_to', 'location', 'description']
+
+class ItineraryUpdate(UpdateView):
+   model = Itinerary
+   fields = '__all__'
+
+class ItineraryDelete(DeleteView):
+   model = Itinerary
+   success_url = '/itineraries'
