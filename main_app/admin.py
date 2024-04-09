b33660be8ea7cd3a1479d35d1639b329ac7ab6c1
jgrey <jjorge5316@hotmail.com>
2024-04-09 14:54:56 -0400

    created list model, urls, and views

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Itinerary
+from .models import Itinerary, List
 
 # Register your models here.
 admin.site.register(Itinerary)
+admin.site.register(List)
---
+++ main_app/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from .models import List
+
+class ListForm(ModelForm):
+  class Meta:
+    model = List
+    fields = ['date', 'time', 'location']
